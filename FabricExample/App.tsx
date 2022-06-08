cfa42cd1bf8b75e2156b87fdc4ef815f119633e3
Jonathan Bones <bonesyblue@gmail.com>
2022-06-08 11:01:35 +0200

    feature(iOS): Call TurboModule from JS

--- FabricExample/App.tsx
+++ FabricExample/App.tsx
@@ -1,11 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Utils } from 'react-native-appjs';
 
 import React from 'react';
 
 export default function App() {
+  const handleGetOrientation = () => {
+    console.log('getOrientation', Utils.getOrientation()); // <- call method from module
+  };
+
   return (
     <View style={styles.container}>
       <Text>App.js</Text>
+      <Button title="getOrientation" onPress={handleGetOrientation} />
     </View>
   );
 }
