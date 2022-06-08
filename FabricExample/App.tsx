2d8e6dcdd7c3e179c89142030d4fa499e32b314a
Jonathan Bones <bonesyblue@gmail.com>
2022-06-08 14:53:46 +0200

    feature(iOS): Render Fabric MapView

--- FabricExample/App.tsx
+++ FabricExample/App.tsx
@@ -1,17 +1,30 @@
 import { Button, StyleSheet, Text, View } from 'react-native';
-import { Utils } from 'react-native-appjs';
+import { MapView, Utils } from 'react-native-appjs';
 
 import React from 'react';
 
 export default function App() {
+  return <FabricExample />;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function TurboModulesExample() {
   const handleGetOrientation = () => {
     console.log('getOrientation', Utils.getOrientation()); // <- call method from module
   };
-
   return (
     <View style={styles.container}>
       <Text>App.js</Text>
       <Button title="getOrientation" onPress={handleGetOrientation} />
+    </View>
+  );
+}
+
+function FabricExample() {
+  return (
+    <View style={styles.container}>
+      <Text>App.js</Text>
+      <MapView style={styles.map} />
     </View>
   );
 }
@@ -21,5 +34,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: 300,
+    height: 300,
   },
 });
